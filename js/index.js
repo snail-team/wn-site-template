e13b1ed94d57c4e15e254ad7eece60d87b18cb64
jiyj <jiyongjia@gmail.com>
2015-07-10 16:09:40 +0800

    init

---
+++ js/index.js
@@ -0,0 +1,43 @@
+/**
+ * author:''
+ */
+
+var PAGE = (function () {
+	var fn = {
+        /**
+         *  add fucntions here
+         *  
+         *      
+         * setPlaceholder:function(container) {
+		 * 			container.find('input').each(function (inx, input) {
+		 * 				var $input = $(input);
+		 * 				if ($input.attr('placeholder')) {
+		 * 					$input.val($input.attr('placeholder'));
+		 * 				}
+		 * 				$input.bind('blur', function () {
+		 * 					if ('' === $input.val()) {
+		 * 						$input.val($input.attr('placeholder'));
+		 * 					}
+		 * 				});
+		 * 				$input.bind('focus', function () {
+		 * 					if ($input.attr('placeholder') === $input.val()) {
+		 * 						$input.val('');
+		 * 					}
+		 * 				});
+		 * 			});
+		 * 		}
+         *  
+        */
+		},
+		init = function () {
+			fn.setPlaceholder($('.message'));
+		};
+	return {
+		fn: fn,
+		init: init
+	}
+})();
+
+$(function () {
+	PAGE.init();
+});
